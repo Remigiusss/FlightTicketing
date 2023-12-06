1b70fa9e80c8f1b7c0e22892e54fc3e69ab61491
Asus <alvinepradana006@gmail.com>
2023-12-06 14:02:20 +0700

    initialization Project

---
+++ src/main/java/com/smk/model/Booking.java
@@ -0,0 +1,35 @@
+package com.smk.model;
+
+public class Booking extends Model {
+    private long id;
+    private long scheduleId;
+    private String name;
+    private double price;
+
+    public void setScheduleId(long l) {
+
+    }
+
+    public void setName(String value) {
+
+    }
+
+    public void setPrice(double v) {
+
+    }
+
+    public long getScheduleId() {
+
+        return 0;
+    }
+
+    public String getName() {
+
+        return null;
+    }
+
+    public double getPrice() {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/smk/model/dto/ScheduleDTO.java
@@ -0,0 +1,52 @@
+package com.smk.model.dto;
+
+import java.util.Date;
+
+public class ScheduleDTO {
+    private long id;
+    private String departureLocation;
+    private String arrivalLocation;
+    private Date departureDate;
+    private String flightNumber;
+
+    public Object getFlightNumber() {
+
+        return null;
+    }
+
+    public String getDepartureLocation() {
+        return null;
+    }
+
+    public String getArrivalLocation() {
+        return null;
+    }
+
+    public char[] getId() {
+        return null;
+    }
+
+    public Object getDepartureDate() {
+        return null;
+    }
+
+    public void setId(int id) {
+
+    }
+
+    public void setDepartureLocation(String departure) {
+
+    }
+
+    public void setArrivalLocation(String arrival) {
+
+    }
+
+    public void setDepartureDate(java.sql.Date departureDate) {
+
+    }
+
+    public void setFlightNumber(String flightNumber) {
+
+    }
+}
